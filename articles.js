645a313f9585eec6de87fb1f1ee255d245b72a57
fathomlesssong <kkrzaqq@gmail.com>
2026-08-11 15:44:15 +0200

    Dodanie centralnej listy artykułów

---
+++ articles.js
@@ -0,0 +1,41 @@
+window.CH82_ARTICLES=[
+  {
+    id:"mlodociany-wyjadacz-owocow",
+    sort:2026081103,
+    date:"11 sierpnia 2026",
+    section:"Społeczność",
+    sectionSlug:"spolecznosc",
+    title:"MŁODOCIANY WYJADACZ OWOCÓW NIE ZWALNIA TEMPA!",
+    summary:"Jeszcze niedawno jego specjalnością były maliny. W ostatni weekend młodociany smakosz oskubał śliwkę do ostatniego owocu.",
+    image:"/assets/chlopiec.png?v=1",
+    alt:"Portret pamięciowy młodocianego wyjadacza owoców",
+    href:"/index.html#mlodociany-wyjadacz-owocow",
+    featured:false
+  },
+  {
+    id:"schody-w-naprawie",
+    sort:2026081102,
+    date:"11 sierpnia 2026",
+    section:"Infrastruktura",
+    sectionSlug:"infrastruktura",
+    title:"SCHODY W NAPRAWIE. ILE MOŻNA NAPRAWIAĆ KILKA STOPNI?",
+    summary:"Naprawa uszkodzonych schodów trwa. I trwa. Bezpieczeństwo jest zapewnione, cierpliwość mieszkańców już trochę mniej.",
+    image:"/assets/schody.png?v=1",
+    alt:"Schody w trakcie naprawy",
+    href:"/index.html#schody-w-naprawie",
+    featured:false
+  },
+  {
+    id:"horror-w-slotwinie",
+    sort:2026081001,
+    date:"10 sierpnia 2026",
+    section:"Wydarzenia",
+    sectionSlug:"wydarzenia",
+    title:"HORROR W SŁOTWINIE! MARTWA MYSZ ZAATAKOWAŁA MIESZKANKĘ NUMERU 82!",
+    summary:"Chciała tylko skorzystać z kontaktu. Chwilę później stanęła oko w oko z koszmarem, którego nie przewidziałby nawet elektryk po trzech kawach.",
+    image:"/assets/martwa-mysz.png?v=4",
+    alt:"Martwa mysz znaleziona za obudową gniazdka elektrycznego w domu numer 82",
+    href:"/articles/2026-08-10-horror-w-slotwinie.html",
+    featured:true
+  }
+];
